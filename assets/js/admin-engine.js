55b9855419eb6e58859e836bb472bdabb589545d
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-04-12 09:01:55 +0100

    Update admin-engine.js

--- assets/js/admin-engine.js
+++ assets/js/admin-engine.js
@@ -1,1 +1,15 @@
+// Admin Control Engine
+   class AdminEngine {
+     constructor() {
+       this.liquidityPool = 0;
+       this.monitorContracts();
+     }
 
+     monitorContracts() {
+       setInterval(() => {
+         if (this.liquidityPool >= 10000) {
+           deployToPancakeSwap();
+         }
+       }, 3600000); // Checks hourly
+     }
+   }
