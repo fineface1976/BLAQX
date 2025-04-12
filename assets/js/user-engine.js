1844f7240f04c5e2407fb68dbf960f4a9452dc0b
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-04-12 08:51:42 +0100

    Update user-engine.js

--- assets/js/user-engine.js
+++ assets/js/user-engine.js
@@ -1,1 +1,20 @@
+// User Trading Engine
+   class UserEngine {
+     constructor() {
+       this.tokenBalance = 0;
+       this.initTrading();
+     }
 
+     initTrading() {
+       setInterval(() => {
+         const profit = this.runArbitrage() + this.runHedgedTrades();
+         this.distributeProfit(profit);
+       }, 300000); // Runs every 5 minutes
+     }
+
+     distributeProfit(profit) {
+       this.tokenBalance += profit * 0.5; // 50% reinvest
+       wallet.cashBalance += profit * 0.4; // 40% company
+       wallet.userProfit += profit * 0.1; // 10% user
+     }
+   }
